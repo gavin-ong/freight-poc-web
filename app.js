27b082195a0fd71c2bc4ae24ef8ae3842de067c1
gavin-ong <gavin.ong@sgp.pillogistics.com>
2026-05-13 15:49:21 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 (function () {
-  // This will only run if app.js is actually executed
-  const el = document.getElementById("jsApp");
+  var el = document.getElementById("jsApp");
   if (el) {
     el.textContent = "JS (app.js): RUNNING ✅";
     el.classList.remove("bad");
     el.classList.add("good");
   }
 })();
+``
